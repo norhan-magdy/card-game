1b3510251798f52e38db3461eac7de4d461d0bc9
Essam-sara <saraessam2580@gmail.com>
2025-01-13 21:18:14 +0200

    add image in intermediate

---
+++ js/intermediate.js
@@ -0,0 +1,30 @@
+// Cards data
+const cardData = [
+    { technology: "shield", image: "images/shield.png", alt: "shield" },
+    { technology: "avengers", image: "images/avengers.png", alt: "avengers" },
+    { technology: "batman", image: "images/batman.png", alt: "batman" },
+    { technology: "black", image: "images/black.png", alt: "black" },
+    { technology: "hulk", image: "images/man.png", alt: "hulk" },
+    { technology: "mjolnir", image: "images/mjolnir.png", alt: "mjolnir" },
+    { technology: "groot", image: "images/baby-groot.png", alt: "groot" },
+    { technology: "dark", image: "images/dark.png", alt: "dark" },
+    { technology: "bat", image: "images/bat.png", alt: "bat" },
+    { technology: "dracula", image: "images/dracula.png", alt: "dracula" },
+    { technology: "witch", image: "images/witch.png", alt: "witch" },
+    { technology: "castle", image: "images/castle.png", alt: "castle" },
+    {
+      technology: "light-saber",
+      image: "images/light-saber.png",
+      alt: "light-saber",
+    },
+    {
+      technology: "harry-potter",
+      image: "images/harry-potter.png",
+      alt: "harry-potter",
+    },
+    { technology: "movie", image: "images/movie.png", alt: "movie" },
+    { technology: "movie1", image: "images/movie (1).png", alt: "movie1" },
+    { technology: "broom", image: "images/broom.png", alt: "broom" },
+    { technology: "scary", image: "images/scary.png", alt: "scary" },
+  ];
+  
